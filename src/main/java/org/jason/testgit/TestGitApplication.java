dbc2674d4899794378c0cbfcc3f4e1c8eeacc153
zhangxi <xi.zhang@ux168.com>
2025-08-29 15:34:35 +0800

    feat: test2

--- src/main/java/org/jason/testgit/TestGitApplication.java
+++ src/main/java/org/jason/testgit/TestGitApplication.java
@@ -12,6 +12,8 @@ public class TestGitApplication {
         System.out.println("Hello World!");
 
         System.out.println("Hello World2!");
+
+        System.out.println("Hello World3!");
     }
 
 }
